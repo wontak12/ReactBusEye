dd48bf01730638657f8228b3a342a72d6183ee29
webdevs-lee <jsweb1008@gmail.com>
2025-02-09 23:59:14 +0900

    first commit

---
+++ src/component/gnb.js
@@ -0,0 +1,20 @@
+
+
+
+function Gnb(){
+    return (
+        <div>
+            <div className="gnbBar">
+                <div className="TRP left">TRP</div>
+                <div className="right">
+                    <div className="alarmConfig">
+                        
+                    </div>
+                    <div className="ImgName"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Gnb
